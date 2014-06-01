57ee378e52082503bd8859a178239421c793a345
techops <fanxiao1987821@gmail.com>
2014-06-01 15:33:30 +0800

    add xsd and endpoint

---
+++ src/main/java/com/erinfan/endpoint/AccountServiceEndpoint.java
@@ -0,0 +1,27 @@
+package com.erinfan.endpoint;
+
+import com.erinfan.webservice.Account;
+import com.erinfan.webservice.AccountDetailsRequest;
+import com.erinfan.webservice.AccountDetailsResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class AccountServiceEndpoint {
+    private static final String TARGET_NAMESPACE = "http://com/blog/samples/webservices/accountservice";
+
+    @PayloadRoot(localPart = "AccountDetailsRequest", namespace = TARGET_NAMESPACE)
+    public
+    @ResponsePayload
+    AccountDetailsResponse getAccountDetails(@RequestPayload AccountDetailsRequest request) {
+        AccountDetailsResponse response = new AccountDetailsResponse();
+        Account account = new Account();
+        account.setAccountBalance(1000);
+        account.setAccountName("ErinFan");
+        account.setAccountNumber("12345");
+        response.setAccountDetails(account);
+        return response;
+    }
+}
