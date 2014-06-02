51ea72b476151b50d7cbb35afb2cf657fa1618dd
techops <fanxiao1987821@gmail.com>
2014-06-02 18:39:03 +0800

    optimize code

--- src/main/java/com/erinfan/endpoint/AccountServiceEndpoint.java
+++ src/main/java/com/erinfan/endpoint/AccountServiceEndpoint.java
@@ -10,7 +10,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class AccountServiceEndpoint {
-    private static final String TARGET_NAMESPACE = "http://com/blog/samples/webservices/accountservice";
+    private static final String TARGET_NAMESPACE = "http://erinfan.webservices.com";
 
     @PayloadRoot(localPart = "AccountDetailsRequest", namespace = TARGET_NAMESPACE)
     public
@@ -18,7 +18,6 @@ public class AccountServiceEndpoint {
     AccountDetailsResponse getAccountDetails(@RequestPayload AccountDetailsRequest request) {
         AccountDetailsResponse response = new AccountDetailsResponse();
         Account account = new Account();
-        account.setAccountBalance(1000);
         account.setAccountName("ErinFan");
         account.setAccountNumber("12345");
         response.setAccountDetails(account);
